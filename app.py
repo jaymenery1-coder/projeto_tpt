62948818ce5ba91cfa06806472da57e0ec60fc9f
Jayme <jaymenery1@gmai.com>
2026-03-20 11:16:26 -0300

    Corrigindo nome do arquivo csv no carregamento de dados

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv('vehicles.csv')
 
 st.header('Análise de Dados de Veículos')
 
